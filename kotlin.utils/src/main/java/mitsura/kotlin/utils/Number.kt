fd3810dca02337abf73a0a630226d6c6ff1e6961
Mitsu <ivanmitsura@gmail.com>
2018-05-24 18:43:59 +0300

    dp to px moved as function

--- kotlin.utils/src/main/java/mitsura/kotlin/utils/Number.kt
+++ kotlin.utils/src/main/java/mitsura/kotlin/utils/Number.kt
@@ -2,5 +2,4 @@ package mitsura.kotlin.utils
 
 import android.content.res.Resources
 
-val Number.DpToPx: Int
-    get() = (this.toInt() * Resources.getSystem().displayMetrics.density).toInt()
+fun Number.DpToPx() = (this.toInt() * Resources.getSystem().displayMetrics.density).toInt()
